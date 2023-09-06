03bfebc008c92debf7871981cd410219436ee643
Roger Graham <roger.graham@tuni.fi>
2023-09-06 11:05:03 +0300

    Next.config.js: specify Strapi image host properties

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: 'export',
+  output: "export",
+  // specify hostname and port of your Strapi backend server to load images from it
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
