b6616cf92fbcb41686b763517d0587627785e606
Roger Graham <roger.graham@tuni.fi>
2023-09-06 11:46:20 +0300

    Replace getFeaturedReview with getReviews(pageSize)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,35 +1,40 @@
 import Link from "next/link";
 import Image from "next/image";
 import Heading from "@/components/Heading";
-import { getFeaturedReview } from "@/lib/reviews";
+import { getReviews } from "@/lib/reviews";
 
 export default async function HomePage() {
-  const review = await getFeaturedReview();
+  const review = await getReviews(3);
   // console.log('[HomePage] rendering');
   return (
     <>
       <Heading>Indie Gamer</Heading>
       <p className="pb-3">Only the best indie games, reviewed for you.</p>
-      <div
-        className="bg-white border rounded shadow w-80
+      <ul className="flex flex-col gap-3">
+        {review.map((review) => (
+          <li
+            key={review.slug}
+            className="bg-white border rounded shadow w-80
                       hover:shadow-xl sm:w-full"
-      >
-        <Link
-          href={`/reviews/${review.slug}`}
-          className="flex flex-col sm:flex-row"
-        >
-          <Image
-            src={review.image}
-            alt=""
-            width="320"
-            height="180"
-            className="rounded-t sm:rounded-l sm:rounded-r-none"
-          />
-          <h2 className="font-orbitron font-semibold py-1 text-center sm:px-2">
-            {review.title}
-          </h2>
-        </Link>
-      </div>
+          >
+            <Link
+              href={`/reviews/${review.slug}`}
+              className="flex flex-col sm:flex-row"
+            >
+              <Image
+                src={review.image}
+                alt="review.title"
+                width="320"
+                height="180"
+                className="rounded-t sm:rounded-l sm:rounded-r-none"
+              />
+              <h2 className="font-orbitron font-semibold py-1 text-center sm:px-2">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
