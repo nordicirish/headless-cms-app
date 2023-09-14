bdea569c0a93fe0f99f284454fa351e14b985c9b
Roger Graham <roger.graham@tuni.fi>
2023-09-14 13:25:46 +0300

    Fix typo

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFoundPage() {
   return (
     <>
       <Heading>Not Found</Heading>
-      <p>Oops, the page you requested dioes not exist.</p>
+      <p>Oops, the page you requested does not exist.</p>
     </>
   );
 }
