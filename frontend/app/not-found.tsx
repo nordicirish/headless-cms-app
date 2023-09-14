90a83621ab488164f0f7cad30f7e4e696891cb6a
Roger Graham <roger.graham@tuni.fi>
2023-09-14 13:25:11 +0300

    Ad not-found.tsx: custom 404 page

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Heading from "@/components/Heading";
+// custom 404 page
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <Heading>Not Found</Heading>
+      <p>Oops, the page you requested dioes not exist.</p>
+    </>
+  );
+}
