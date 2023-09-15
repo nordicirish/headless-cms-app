6feb7c9b4754a278e094c73f1932578d85561aa7
Roger Graham <roger.graham@tuni.fi>
2023-09-15 11:43:27 +0300

    Add revalidation if tag is passed

---
+++ frontend/app/webhooks/cms-event/route.tsx
@@ -0,0 +1,14 @@
+// webhook created on strapi notifies next of changes to cms
+
+import { revalidateTag } from "next/cache";
+import { NextRequest } from "next/server";
+import { CACHE_TAG_REVIEWS } from "@/lib/reviews";
+export async function POST(request: NextRequest) {
+  const payload = await request.json();
+  if (payload.model === "review") {
+    revalidateTag(CACHE_TAG_REVIEWS);
+    console.log("Revalidated:", CACHE_TAG_REVIEWS);
+  }
+
+  return new Response(null, { status: 204 });
+}
