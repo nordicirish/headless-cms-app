38cdb6747cb198d7f92496522612694241ec1342
Roger Graham <roger.graham@tuni.fi>
2023-09-20 15:17:52 +0300

    Add dummy review data

--- frontend/components/SearchBox.tsx
+++ frontend/components/SearchBox.tsx
@@ -3,6 +3,14 @@
 import { Combobox } from "@headlessui/react";
 import { useIsClient } from "@/lib/hooks";
 
+const reviews = [
+  { slug: "hades-2018", title: "Hades" },
+  { slug: "fall-guys", title: "Fall Guys: Ultimate Knockout" },
+  { slug: "black-mesa", title: "Black Mesa" },
+  { slug: "disco-elysium", title: "Disco Elysium" },
+  { slug: "dead-cells", title: "Dead Cells" },
+];
+
 export default function SearchBox() {
   const isClient = useIsClient();
   if (!isClient) {
