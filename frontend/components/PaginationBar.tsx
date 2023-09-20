27c7c4863d10235e6c82211238b74da5f558eb78
Roger Graham <roger.graham@tuni.fi>
2023-09-20 13:00:40 +0300

    Change left annd right chevrons to heroicons

--- frontend/components/PaginationBar.tsx
+++ frontend/components/PaginationBar.tsx
@@ -1,12 +1,17 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
 export default function PaginationBar({ page, pageCount, href }) {
   return (
-    <div className="flex gap-2 pb-3">
-      <Link href={`${href}?page=${page - 1}`}>&lt;</Link>
+    <div className="flex gap-2 items-center pb-3">
+      <Link href={`${href}?page=${page - 1}`}>
+        <ChevronLeftIcon className="h-5 w-5" />
+      </Link>
       <span>
         Page {page} of {pageCount}
       </span>
-      <Link href={`${href}?page=${page + 1}`}>&gt;</Link>
+      <Link href={`${href}?page=${page + 1}`}>
+        <ChevronRightIcon className="h-5 w-5" />
+      </Link>
     </div>
   );
 }
