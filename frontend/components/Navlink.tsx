8726cce2643501445a5585c4f2df37bb6a18b43d
Roger Graham <roger.graham@tuni.fi>
2023-09-20 14:06:26 +0300

    Add conditional rendering to make navlink unclickable if on current page

--- frontend/components/Navlink.tsx
+++ frontend/components/Navlink.tsx
@@ -1,5 +1,7 @@
+"use client";
 import type { ReactNode } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export interface NavLinkProps {
   children: ReactNode;
@@ -7,6 +9,11 @@ export interface NavLinkProps {
   prefetch?: boolean;
 }
 export default function NavLink({ href, children, prefetch }: NavLinkProps) {
+  //makes the link unclickable if it is the current page
+  const pathName = usePathname();
+  if (href === pathName) {
+    return <span className="text-orange-800">{children}</span>;
+  }
   return (
     <Link
       href={href}
