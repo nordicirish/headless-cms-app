24d6fb4ce18e932dbf1baf7047a04172a1799df3
Roger Graham <roger.graham@tuni.fi>
2023-09-20 13:19:16 +0300

    Add conditional chevron link rendering

--- frontend/components/PaginationBar.tsx
+++ frontend/components/PaginationBar.tsx
@@ -3,19 +3,30 @@ import Link from "next/link";
 export default function PaginationBar({ page, pageCount, href }) {
   return (
     <div className="flex gap-2 items-center pb-3">
-      <PaginationLink href={`${href}?page=${page - 1}`}>
+      <PaginationLink href={`${href}?page=${page - 1}`} enabled={page > 1}>
         <ChevronLeftIcon className="h-5 w-5" />
       </PaginationLink>
       <span>
         Page {page} of {pageCount}
       </span>
-      <PaginationLink href={`${href}?page=${page + 1}`}>
+      <PaginationLink
+        href={`${href}?page=${page + 1}`}
+        enabled={page < pageCount}
+      >
         <ChevronRightIcon className="h-5 w-5" />
       </PaginationLink>
     </div>
   );
 }
-function PaginationLink({ href, children }) {
+function PaginationLink({ href, enabled, children }) {
+  if (!enabled) {
+    return (
+        // cursor-not-allowed prevents the user from clicking the link
+      <span className="border rounded cursor-not-allowed text-slate-300 text-sm">
+        {children}
+      </span>
+    );
+  }
   return (
     <Link
       href={href}
