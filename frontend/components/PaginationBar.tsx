aeb7c6347fda4fcec3d63ac2df55a13746175dda
Roger Graham <roger.graham@tuni.fi>
2023-09-20 13:07:16 +0300

    Add PaginationLink({href,  children}) to apply common styles

--- frontend/components/PaginationBar.tsx
+++ frontend/components/PaginationBar.tsx
@@ -3,15 +3,25 @@ import Link from "next/link";
 export default function PaginationBar({ page, pageCount, href }) {
   return (
     <div className="flex gap-2 items-center pb-3">
-      <Link href={`${href}?page=${page - 1}`}>
+      <PaginationLink href={`${href}?page=${page - 1}`}>
         <ChevronLeftIcon className="h-5 w-5" />
-      </Link>
+      </PaginationLink>
       <span>
         Page {page} of {pageCount}
       </span>
-      <Link href={`${href}?page=${page + 1}`}>
+      <PaginationLink href={`${href}?page=${page + 1}`}>
         <ChevronRightIcon className="h-5 w-5" />
-      </Link>
+      </PaginationLink>
     </div>
+  );
+}
+function PaginationLink({ href, children }) {
+  return (
+    <Link
+      href={href}
+      className="border rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
+    >
+      {children}
+    </Link>
   );
 }
