ed7189cbed31590d9ebd8889881650c7d623c136
Roger Graham <roger.graham@tuni.fi>
2023-09-21 14:45:15 +0300

    Change const filtered: slice(=0,5) Only first 5 reviews in search box

--- frontend/components/SearchBox.tsx
+++ frontend/components/SearchBox.tsx
@@ -2,30 +2,43 @@
 //need to use client because is rendered on the client side
 import { Combobox } from "@headlessui/react";
 import { useIsClient } from "@/lib/hooks";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-const reviews = [
-  { slug: "hades-2018", title: "Hades" },
-  { slug: "fall-guys", title: "Fall Guys: Ultimate Knockout" },
-  { slug: "black-mesa", title: "Black Mesa" },
-  { slug: "disco-elysium", title: "Disco Elysium" },
-  { slug: "dead-cells", title: "Dead Cells" },
-];
-
-export default function SearchBox() {
+export default function SearchBox({ reviews }) {
+  // useRouter is a hook that gives us access to the router object
+  const router = useRouter();
   const isClient = useIsClient();
+  const [query, setQuery] = useState("");
+
+  const handleChange = (review) => {
+    router.push(`/reviews/${review.slug}`);
+  };
+  //   console.log("[SearchBox] query:", query);
+
+  // prevents search box from rendering on the server
   if (!isClient) {
     return null;
   }
+
+  const filtered = reviews
+    .filter((review) =>
+      review.title.toLowerCase().includes(query.toLowerCase())
+    )
+    .slice(0, 5);
+  //only return first 5 results in the search box
   return (
     <div className="relative w-48">
-      <Combobox>
+      <Combobox onChange={handleChange}>
         <Combobox.Input
           placeholder="Search…"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
           className="border px-2 py-1 rounded w-full"
         />
         <Combobox.Options className="absolute bg-white py-1 w-full">
-          {reviews.map((review) => (
-            <Combobox.Option key={review.slug} value={review.slug}>
+          {filtered.map((review) => (
+            <Combobox.Option key={review.slug} value={review}>
               {({ active }) => (
                 <span
                   className={`block px-2 truncate w-full ${
