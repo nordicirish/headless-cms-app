993898f7246a70452b63857d96057648635b415b
Roger Graham <roger.graham@tuni.fi>
2023-09-22 11:40:52 +0300

    Add toRemotePattern(): rewrite strapi image urls to match the conventions Next.js expects

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
   // output: "export",
   // specify hostname and port of your Strapi backend server to load images from it
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
-    ],
+    remotePatterns: [toRemotePattern(process.env.CMS_IMAGE_PATTERN)],
   },
 };
+// 
+function toRemotePattern(urlString) {
+  const url = new URL(urlString);
+  return {
+    protocol: url.protocol.replace(":", ""),
+    hostname: url.hostname,
+    port: url.port,
+    pathname: url.pathname,
+  };
+}
